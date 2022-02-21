86aa81524dfad9c54b1fd15dcdfe7b0032f2bc1b
Thiago Araujo <thiago72araujo@gmail.com>
2022-02-20 21:28:38 -0300

    alteração da url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL_BACKEND: 'https://json-server-teste.herokuapp.com/',
+  BASE_URL_BACKEND: 'https://json-server-cod3r.herokuapp.com/',
 };
